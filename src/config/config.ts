f2280ebf22ffd428074c42c8297f766ed27357c3
nenertiy <nenertiy@gmail.com>
2025-05-06 22:31:35 +0300

    refactor: remove PORT configuration and simplify app initialization

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,4 @@
 export default () => ({
-  PORT: parseInt(process.env.PORT) || 3000,
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
   ALLOWED_CHAT_ID: parseInt(process.env.ALLOWED_CHAT_ID),
   GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
